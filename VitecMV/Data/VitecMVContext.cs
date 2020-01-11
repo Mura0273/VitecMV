460a8b4d88e70f13e8fd1889b19f33aea347cf1d
Murad Ali Ahmad Hassan <mura0273@edu.eal.dk>
2020-01-12 00:15:00 +0100

    Add project files.

---
+++ VitecMV/Data/VitecMVContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VitecMV.Models;
+
+namespace VitecMV.Data
+{
+    public class VitecMVContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public VitecMVContext(DbContextOptions<VitecMVContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Products> Products { get; set; }
+    }
+}
---
+++ VitecMV/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VitecMV.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
